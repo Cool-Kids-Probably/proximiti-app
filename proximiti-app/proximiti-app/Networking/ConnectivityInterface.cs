dd7ec2d7a8883afc941cb7940addd9e6d81e2837
yiclemence <yiclemence@gmail.com>
2020-07-28 22:28:01 -0700

    Various changes to basic structure, added ConnectivityInterface to provide basic structure for connection and data transportation.

---
+++ proximiti-app/proximiti-app/Networking/ConnectivityInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proximiti.Networking
+{
+    interface ConnectivityInterface
+    {
+        async Task<int> Send(String[] args, Byte[] data)
+        {
+            
+        }
+
+    }
+}
